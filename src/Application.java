4be74454a70e5dc6697cb3c20ed5176c89858a51
Desislava-A <39766396+Desislava-A@users.noreply.github.com>
2019-05-08 20:17:50 +0300

    updating Application class: added method to print the logo row by row

--- src/Application.java
+++ src/Application.java
@@ -98,6 +98,112 @@ public class Application {
 
 
     private void printLogo(int n) {
-        //add logic for printing the logo with thickness N
+        //will store rows of logo as String elements in String array
+        String[] rowsToPrint = new String[n + 1];
+        int rowNumber = 0;
+
+        //Creating strings for the rows in the upper part of first M letter from the logo
+        for (rowNumber = 0; 2 * rowNumber <= n - 1; rowNumber++) {
+            int starsCount = n + 2 * rowNumber;
+            int dashesStart = n - rowNumber;
+            int dashesMiddle = n - 2 * rowNumber;
+            StringBuilder builder = new StringBuilder();
+
+            for (int star = 1; star <= starsCount; star++) {
+                builder.append("*");
+            }
+
+            String starsBlock = builder.toString();
+            builder.setLength(0);
+
+            for (int dash = 1; dash <= dashesStart; dash++) {
+                builder.append("-");
+            }
+
+            String dashesAtStart = builder.toString();
+            builder.setLength(dashesMiddle);
+
+            String middleDashes = builder.toString();
+            builder.setLength(0);
+            builder.append(dashesAtStart);
+            builder.append(starsBlock);
+            builder.append(middleDashes);
+            builder.append(starsBlock);
+            builder.append(dashesAtStart);
+
+            String halfRow = builder.toString();
+            builder.append(halfRow);
+            rowsToPrint[rowNumber] = builder.toString();
+            builder.setLength(0);
+
+        }
+
+        /* Creating strings for the rows from down half part of the logo;
+         * String nStars is used to store sequence of N star symbols.
+         * Within the loop for each row are calculated, sequences of free area symbols and
+         * symbols for the biggest sequence of stars in the center of the M letter.
+         **/
+
+        StringBuilder builder = new StringBuilder();
+        for (int star = 1; star <= n; star++) {
+            builder.append("*");
+        }
+        String nStars = builder.toString();
+        builder.setLength(0);
+
+
+        int middleDashes = 1;
+
+        for (int row = rowNumber; row <= n; row++) {
+
+
+            for (int star = 1; star <= 2 * n - middleDashes; star++) {
+                builder.append("*");
+            }
+
+            String middleStars = builder.toString();
+
+            builder.setLength(0);
+
+            for (int dash = 1; dash <= n - row; dash++) {
+                builder.append("-");
+            }
+
+            String dashesAtStartAndEnd = builder.toString();
+            builder.setLength(0);
+
+            for (int dash = 1; dash <= middleDashes; dash++) {
+                builder.append("-");
+            }
+
+            String interimDashes = builder.toString();
+            builder.setLength(0);
+
+            builder.append(dashesAtStartAndEnd);
+            builder.append(nStars);
+            builder.append(interimDashes);
+            builder.append(middleStars);
+            builder.append(interimDashes);
+            builder.append(nStars);
+            builder.append(dashesAtStartAndEnd);
+
+            String rowHalf = builder.toString();
+            builder.append(rowHalf);
+
+
+            rowsToPrint[row] = builder.toString();
+            builder.setLength(0);
+
+            middleDashes += 2;
+
+
+        }
+
+        //printing the logo row by row
+        System.out.println();
+        for (String rowStr : rowsToPrint
+                ) {
+            System.out.println(rowStr);
+        }
     }
 }
