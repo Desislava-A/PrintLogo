5340bb735ea8c26145395b7d6bd9525f44bbc722
Desislava-A <39766396+Desislava-A@users.noreply.github.com>
2019-05-08 18:11:38 +0300

    updating Application class: added two methods to display main menu

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,19 @@
 public class Application {
 
-public void runMenu(){
+    public void runMenu() {
 
-}
+    }
+
+    private void printWelcomeNote() {
+        System.out.println("+-----------------------------------------------+");
+        System.out.println("+          Welcome to my application            +");
+        System.out.println("+     that prints MENTORMATE logo on screen     +");
+        System.out.println("+-----------------------------------------------+");
+    }
+
+    private void showMainMenu() {
+        System.out.println("\n   ++   To PRINT the logo, please enter:        1");
+        System.out.println("   ++   To EXIT the application, please enter:  0");
+    }
 
 }
